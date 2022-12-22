29f8fba6c92d00c3021566ff68c50cb71b485fc5
Jh <jh_eipx@outlook.com>
2022-12-22 18:35:52 -0500

    sql_update_2

--- data_pipeline.py
+++ data_pipeline.py
@@ -4,8 +4,10 @@ from credentials import *
 from datetime import datetime, timedelta
 from time import sleep
 from sqlalchemy import create_engine
+from pytz import timezone
 import traceback
 
+market_timezone = timezone("Asia/Shanghai")
 ts.set_token(my_token)
 pro = ts.pro_api()
 my_conn = create_engine("mysql+pymysql://"+my_user+":"+my_password+"@localhost/qt_database")
@@ -17,16 +19,19 @@ class index_pipeline:
         self.data_path = data_path
 
     def get_index_list(self):
-        today = datetime.today().strftime("%Y%m%d")
+        today = datetime.now(market_timezone).strftime("%Y%m%d")
         df = pro.index_weight(index_code=self.index_code, start_date=self.start_date, end_date=today)
-        df = df[["con_date"]].rename({"con_code":"Index"}, axis=1)
+        df = df[["con_date"]].rename({"con_code":"ts_code"}, axis=1)
         df.to_csv(self.data_path+'universe', dtype=str, index=False)
 
     def get_index_history_daily(self):
+        # empty the old history first
+        my_conn.execute("TRUNCATE TABLE daily_prices")
+
         # get the stock ID list
         stock_list = pd.read_csv(self.data_path+'universe', dtype=str)
         count = 0
-        today = datetime.today().strftime("%Y%m%d")
+        today = datetime.now(market_timezone).strftime("%Y%m%d")
         for id in stock_list["Index"]:
             count += 1
             while True:
@@ -42,38 +47,38 @@ class index_pipeline:
                     print("fail to get id: "+id+", repeat now...")
                     sleep(10)
                     continue
-                
-        print("job_done! ")
+        self.setup = True  
+        print("job done!")
     
     def update_hs300_daily(self):
-        # read stock list file from universe
-        today = datetime.today().strftime("%Y%m%d")
-        stock_list = pd.read_csv(self.data_path+'universe', dtype=str)
-        count = 0
-        close_market_date = []
-        for id in stock_list["Index"]:
-            df = pd.read_csv(self.data_path+"daily_price/"+str(id)+".csv", dtype=str)  
-            count += 1
-            # if the dataframe is empty, assign the start date to most recent date
-            if len(df) == 0:
-                most_recent_date = datetime.strptime(self.start_date, "%Y%m%d")
-            else:
-                most_recent_date = datetime.strptime(df["trade_date"].iloc[0], "%Y%m%d")
-            
-            # check if all the data are up to date
-            first_day = most_recent_date + timedelta(days=1)
-            if  first_day not in close_market_date and first_day < datetime.today():
-                lines_append = pro.daily(ts_code=id, start_date=first_day.strftime("%Y%m%d"), end_date=today)
-                if len(lines_append) != 0:
-                    lines_append = lines_append[["ts_code", "trade_date", "open", "close", "low", "high"]]
-                    print(count, "updating id", id, "from", lines_append["trade_date"].iloc[-1], "to", today)
-                    new_df = pd.concat([lines_append, df], ignore_index=True, axis=0)
-                    new_df.to_csv(self.data_path+"daily_price/"+str(id)+'.csv', index=False)
-                    continue
-                else:
-                    close_market_date.append(first_day)
-            print(count, "id", id, "is already up to date")
-        #print_stats()
+        # get next date to update date
+        query = "SELECT MAX(trade_date) AS date FROM daily_prices"
+        today = datetime.now(market_timezone).date()
+        today_str = today.strftime("%Y%m%d")
+        next_update_date = (pd.read_sql(query, my_conn))["date"][0] + timedelta(days=1)
+        next_update_str = next_update_date.strftime("%Y%m%d")
+        
+        # check if the database is already up to date
+        if next_update_date < today:
+            duration = (today - next_update_date).days
+
+            # just to bypass records per minute restriction, can be removed if the token is available
+            if duration > 20:
+                print("The date diff from last update date to today exceeds 20 days, please re-initialize the history data")
+                return
+            open_info = pro.query("trade_cal", start_date=next_update_str, end_date=today_str)
+            stock_list = ""
+            if len(open_info) != 0 and (1 in open_info["is_open"]):
+                query = "SELECT ts_code FROM universe"
+                stock_list_df = pd.read_sql(query, my_conn)
+                for i in range(len(stock_list_df)):
+                    stock_list += stock_list_df["ts_code"].iloc[i] + ","
+                new_info = pro.daily(ts_code=stock_list[:-1], start_date=next_update_str, end_date=today_str)
+                new_info = new_info[["ts_code", "trade_date", "open", "close", "low", "high"]]
+                new_info.to_sql(con=my_conn, name="daily_prices", if_exists="append", index=False)
+                print("Database successfully updated!")
+                return
+        print("Database is already up to date!")
             
         
 if __name__ == "__main__":
